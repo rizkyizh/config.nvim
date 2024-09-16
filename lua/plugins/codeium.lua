847f73b0783d94cdd240ef2e694f7e744f0bcdf4
rizkyizh <rizkiizzulhaq14@gmail.com>
2024-09-16 12:40:06 +0700

    `fix`: codeium binding

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -4,7 +4,8 @@ return {
   event = "BufEnter",
   config = function()
     vim.keymap.set("i", "<c-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#Cycle"]() end, { expr = true })
+    vim.keymap.set("i", "<c-'>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
     vim.keymap.set("i", "<c-_>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
   end,
 }
