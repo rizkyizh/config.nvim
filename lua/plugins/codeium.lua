de71db09bb44ca2d161218563861d1d82ff17267
rizkyizh <rizkiizzulhaq14@gmail.com>
2025-04-26 14:57:32 +0700

    git init

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,11 @@
+return {
+
+  "Exafunction/codeium.vim",
+  event = "BufEnter",
+  config = function()
+    vim.keymap.set("i", "<c-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+    vim.keymap.set("i", "<c-'>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+    vim.keymap.set("i", "<c-_>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+  end,
+}
