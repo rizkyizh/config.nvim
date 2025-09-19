7ce1a9e7c5174610d4fa28fddfd71f898064059c
rizkyizh <rizkiizzulhaq14@gmail.com>
2025-09-19 23:01:12 +0700

    `fix`: separator left on statusline

--- lua/plugins/nvchad_statusline.lua
+++ lua/plugins/nvchad_statusline.lua
@@ -20,7 +20,7 @@ return {
           -- left = { "", "▉" }, -- separator for the left side of the statusline
           -- left = { "", "" }, -- separator for the left side of the statusline
           left = { "", "" }, -- separator for the left side of the statusline
-          right = { " ", "" }, -- separator for the right side of the statusline
+          right = { " ▐", "" }, -- separator for the right side of the statusline
           tab = { "", "" },
         },
         -- add new colors that can be used by heirline
