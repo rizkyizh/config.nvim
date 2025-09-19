1590fe6e6cb63219811982008edc935793577d0b
rizkyizh <rizkiizzulhaq14@gmail.com>
2025-09-19 22:28:41 +0700

    `feat`: dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,27 +1,39 @@
 ---@type LazySpec
 return {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      -- opts.section.header.val = {
-      --   " █████  ███████ ████████ ██████   ██████",
-      --   "██   ██ ██         ██    ██   ██ ██    ██",
-      --   "███████ ███████    ██    ██████  ██    ██",
-      --   "██   ██      ██    ██    ██   ██ ██    ██",
-      --   "██   ██ ███████    ██    ██   ██  ██████",
-      --   " ",
-      --   "    ███    ██ ██    ██ ██ ███    ███",
-      --   "    ████   ██ ██    ██ ██ ████  ████",
-      --   "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-      --   "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-      --   "    ██   ████   ████   ██ ██      ██",
-      -- }
-      opts.section.header.val = {
-        " ▗▄▖ ▗▖  ▗▖▗▄▄▖  ▗▄▖ ▗▄▄▄▖▗▖ ▗▖▗▖  ▗▖▗▄▄▄▖▗▖  ▗▖",
-        "▐▌ ▐▌▐▛▚▞▜▌▐▌ ▐▌▐▌ ▐▌  █  ▐▌ ▐▌▐▌  ▐▌  █  ▐▛▚▞▜▌",
-        "▐▛▀▜▌▐▌  ▐▌▐▛▀▚▖▐▛▀▜▌  █  ▐▌ ▐▌▐▌  ▐▌  █  ▐▌  ▐▌",
-        "▐▌ ▐▌▐▌  ▐▌▐▙▄▞▘▐▌ ▐▌  █  ▝▚▄▞▘ ▝▚▞▘ ▗▄█▄▖▐▌  ▐▌",
-      }
-      return opts
-    end,
+  "goolord/alpha-nvim",
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      "",
+      "",
+      "",
+      " ▗▄▖ ▗▖  ▗▖▗▄▄▖  ▗▄▖ ▗▄▄▄▖▗▖ ▗▖▗▖  ▗▖▗▄▄▄▖▗▖  ▗▖",
+      "▐▌ ▐▌▐▛▚▞▜▌▐▌ ▐▌▐▌ ▐▌  █  ▐▌ ▐▌▐▌  ▐▌  █  ▐▛▚▞▜▌",
+      "▐▛▀▜▌▐▌  ▐▌▐▛▀▚▖▐▛▀▜▌  █  ▐▌ ▐▌▐▌  ▐▌  █  ▐▌  ▐▌",
+      "▐▌ ▐▌▐▌  ▐▌▐▙▄▞▘▐▌ ▐▌  █  ▝▚▄▞▘ ▝▚▞▘ ▗▄█▄▖▐▌  ▐▌",
+      "",
+      "",
+      "",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("SPC f f", "  Find file"),
+      dashboard.button("SPC f o", "  Recently used files"),
+      dashboard.button("SPC f w", "  Find text"),
+      dashboard.button("SPC f '", "  Bookmarks"),
+      dashboard.button("SPC S l", "  Last Session", "<Leader>Sl"),
+      dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    }
+
+    dashboard.section.footer.val = {
+      "",
+      "https://github.com/rizkyizh/config.nvim",
+      "",
+    }
+    return dashboard.opts
+  end,
 }
+
