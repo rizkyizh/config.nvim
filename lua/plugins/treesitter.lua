4163dd865b28528ae182a51548717a4a6b9c041d
rizkyizh <rizkiizzulhaq14@gmail.com>
2025-09-24 10:49:03 +0700

    `fix`: treesitter plug depreceted

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,18 @@
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   opts = {
     ensure_installed = {
       "lua",
       "vim",
-      "tsx",         -- buat React TSX
-      "typescript",  -- TypeScript
-      "javascript",  -- JavaScript
-      "html",        -- HTML (kadang dipakai di JSX/TSX)
+      "tsx",
+      "typescript",
+      "javascript",
+      "html",
     },
     highlight = { enable = true },
     indent = { enable = true },
-    autotag = { enable = true }, -- penting untuk auto close tag
   },
 }
 
